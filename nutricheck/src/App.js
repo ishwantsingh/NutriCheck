071617645c3cdc6a6a0ccd8858580205d5b29c3f
Ishwant Singh <ishwant8@gmail.com>
2019-10-14 22:17:41 +0530

    update: firestore connected to app.js

--- nutricheck/src/App.js
+++ nutricheck/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 import Options from "./components/Options";
 import Headbar from "./components/Headbar";
@@ -18,4 +21,15 @@ class App extends React.Component {
   }
 }
 
-export default App;
+//export default App;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    dishes: state.firestore.ordered.dishes
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "dishes" }])
+)(App);
--- nutricheck/src/index.js
+++ nutricheck/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore } from "redux-firestore";
-import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import fbConfig from "./config/fbConfig";
-import rootReducer from "./state/reducers/rootReducer";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import { reduxFirestore, getFirestore } from "redux-firestore";
+// import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+// import fbConfig from "./config/fbConfig";
+// import rootReducer from "./state/reducers/rootReducer";
 
 // const store = createStore(
 //   rootReducer,
@@ -17,27 +17,79 @@ import rootReducer from "./state/reducers/rootReducer";
 //     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
 //     reactReduxFirebase(fbConfig, {
 //       userProfile: "users",
-//       useFirestoreForProfile: true,
+//       userFirestoreForProfile: true,
 //       attachAuthIsReady: true
 //     }),
-//     reduxFirestore(fbConfig) // redux bindings for firestore
-//     //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     reduxFirestore(fbConfig)
+//   )
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //used for redux devtools
+// );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./state/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+import * as serviceWorker from "./serviceWorker";
+
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     // reactReduxFirebase(fbConfig),
+//     reduxFirestore(fbConfig), // redux bindings for firestore
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 //   )
 // );
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //used for redux devtools
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(fbConfig, {
+      userProfile: "users",
+      useFirestoreForProfile: true,
+      attachAuthIsReady: true
+    }),
+    reduxFirestore(fbConfig), // redux bindings for firestore
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+});
+
 serviceWorker.unregister();
